3bc244a50bffe85998fb54316841be321af28c92
Kadir Harmanda <43813662+kddr@users.noreply.github.com>
2022-02-05 21:02:26 +0300

    Create polindrom.java

---
+++ polindrom.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String sonuc ="";
+
+        for(int i = 10; i<=100; i++)
+        {
+            Long sayi = Long.parseLong(String.valueOf(i));
+            String tmpSonuc = "";
+
+            tmpSonuc += "İlk Sayı: " + sayi;
+            while(!sayi.equals(tersiniBul(sayi)))
+            {
+                tmpSonuc += "\nTersi: " + tersiniBul(sayi);
+                sayi += tersiniBul(sayi);
+                tmpSonuc += "\nYeni Sayı: " + sayi;
+            }
+
+            if(tmpSonuc.length() > sonuc.length())
+                sonuc = tmpSonuc;
+        }
+
+        System.out.println(sonuc);
+    }
+
+    private static Long tersiniBul(Long sayi) {
+
+        Long tersSayi = Long.valueOf(0);
+        while (sayi > 0) {
+            tersSayi = (tersSayi * 10) + (sayi % 10);
+            sayi /= 10;
+        }
+        return tersSayi;
+    }
+}
